7cd5afe01b05b64efc041400ebaeefa5514164c2
Sydney B <BestLemon555@gmail.com>
2022-08-16 18:44:56 -0700

    Added comment

--- FixingMinnie/src/main/java/frc/robot/Robot.java
+++ FixingMinnie/src/main/java/frc/robot/Robot.java
@@ -47,6 +47,7 @@ public class Robot extends TimedRobot {
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
+  //added comment lol
   @Override
   public void disabledInit() {}
 
