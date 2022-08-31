6f0e97dfec6ef542080e0dd0fab4ee99d1458e1b
Sydney B <BestLemon555@gmail.com>
2022-08-30 19:58:48 -0700

    Up to 6:32 in 0 to Auto Swerve vid

---
+++ AZRL9413/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.AnalogInput;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SwerveModule extends SubsystemBase {
+  /** Creates a new SwerveModule. */
+  
+  public SwerveModule() { }
+  //Module 1
+  private final CANSparkMax driveMotor;
+  private final CANSparkMax turningMotor;
+  
+  private final RelativeEncoder driveEncoder;
+  private final RelativeEncoder turningEncoder;
+  
+  private final PIDController turningPidController;
+  
+  private final AnalogInput absoluteEncoder;
+  private final boolean absoluteEncoderReversed;
+  private final double absoluteEncoderOffsetRad;
+
+  public SwerveModule(int driveMotorId, int turningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
+  int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+
+    this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+    this.absoluteEncoderReversed = absoluteEncoderReversed;
+    absoluteEncoder = new AnalogInput(absoluteEncoderId);
+
+    driveMotor = new CANSparkMax(driveMotorId, MotorType.kBrushless);
+    turningMotor = new CANSparkMax(turningMotorId, MotorType.kBrushless);
+
+    driveMotor.setInverted(driveMotorReversed);
+    turningMotor.setInverted(turningMotorReversed);
+
+    driveEncoder = driveMotor.getEncoder();
+    turningEncoder = turningMotor.getEncoder();
+
+    driveEncoder.setPositionConversionFactor(ModuleConstants.kDriveEncoderRot2Meter);
+    driveEncoder.setVelocityConversionFactor(ModuleConstants.kDriveEncoderRPM2MeterPerSec);
+    turningEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderRot2Rad);
+    turningEncoder.setVelocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
+
+    turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0);
+    turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+
+    resetEncoders();
+} 
+  }
+  //Module 2
+//Module 3
+//Module 4
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
