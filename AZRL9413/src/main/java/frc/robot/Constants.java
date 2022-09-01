b896b80bcb0db9e251f5777fe311aab671b2ee79
Sydney B <BestLemon555@gmail.com>
2022-08-31 18:45:07 -0700

    Created Function to Define Swerve Modules

--- AZRL9413/src/main/java/frc/robot/Constants.java
+++ AZRL9413/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,17 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    //need to update values
+    public static final class ModuleConstants {
+        public static final double kWheelDiameterMeters = 4/39.37;
+        public static final double kDriveMotorGearRatio = 1 / 5.8462;
+        public static final double kTurningMotorGearRatio = 1 / 18.0;
+        public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+        public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+        public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+        public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+        public static final double kPTurning = 0.5;
+    }
+
+}
--- AZRL9413/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ AZRL9413/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -4,30 +4,33 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.AnalogInput;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.*;
+import edu.wpi.first.wpilibj.RobotController;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class SwerveModule extends SubsystemBase {
+public class SwerveModule {
   /** Creates a new SwerveModule. */
   
-  public SwerveModule() { }
+  // public SwerveModule() { }
   //Module 1
   private final CANSparkMax driveMotor;
   private final CANSparkMax turningMotor;
-  
+
   private final RelativeEncoder driveEncoder;
   private final RelativeEncoder turningEncoder;
-  
+
   private final PIDController turningPidController;
-  
+
   private final AnalogInput absoluteEncoder;
   private final boolean absoluteEncoderReversed;
   private final double absoluteEncoderOffsetRad;
@@ -39,6 +42,7 @@ public class SwerveModule extends SubsystemBase {
     this.absoluteEncoderReversed = absoluteEncoderReversed;
     absoluteEncoder = new AnalogInput(absoluteEncoderId);
 
+    
     driveMotor = new CANSparkMax(driveMotorId, MotorType.kBrushless);
     turningMotor = new CANSparkMax(turningMotorId, MotorType.kBrushless);
 
@@ -57,14 +61,18 @@ public class SwerveModule extends SubsystemBase {
     turningPidController.enableContinuousInput(-Math.PI, Math.PI);
 
     resetEncoders();
-} 
   }
-  //Module 2
-//Module 3
-//Module 4
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  public double getAbsoluteEncoderRad() {
+    double angle = absoluteEncoder.getVoltage() / RobotController.getVoltage5V();
+    angle *= 2.0 * Math.PI;
+    angle -= absoluteEncoderOffsetRad;
+    return angle * (absoluteEncoderReversed ? -1.0 : 1.0);
+}
+  public void resetEncoders() {
+  driveEncoder.setPosition(0);
+  turningEncoder.setPosition(getAbsoluteEncoderRad());
+  } 
+ 
+
 }
