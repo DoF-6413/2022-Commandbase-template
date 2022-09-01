2ba7f7b3c17474df5de13f5a70948eaa823eb6f2
Sydney B <BestLemon555@gmail.com>
2022-08-31 19:29:39 -0700

    Added comments

--- AZRL9413/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ AZRL9413/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveDriveSubsystem extends SubsystemBase {
   /** Creates a new SwerveDriveSubsystem. */
+  //Establishes specific Swerve Modules per Motor Mounting
   private final SwerveModule frontLeft = new SwerveModule(
     DriveConstants.kFrontLeftDriveMotorPort,
     DriveConstants.kFrontLeftTurningMotorPort,
