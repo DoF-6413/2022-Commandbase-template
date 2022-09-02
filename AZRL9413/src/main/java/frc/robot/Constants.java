d162a7733794be3cd9aac01e8ecff753f9a5b966
Sydney B <BestLemon555@gmail.com>
2022-09-01 19:23:07 -0700

    Added Constants for the Controller

--- AZRL9413/src/main/java/frc/robot/Constants.java
+++ AZRL9413/src/main/java/frc/robot/Constants.java
@@ -83,5 +83,14 @@ public final class Constants {
         public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 3;
         public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = 3;
     }
+    public static final class OIConstants {
+        public static final int kDriverControllerPort = 0;
 
+        public static final int kDriverYAxis = 1;
+        public static final int kDriverXAxis = 0;
+        public static final int kDriverRotAxis = 4;
+        public static final int kDriverFieldOrientedButtonIdx = 1;
+
+        public static final double kDeadband = 0.05;
+    }
 }
