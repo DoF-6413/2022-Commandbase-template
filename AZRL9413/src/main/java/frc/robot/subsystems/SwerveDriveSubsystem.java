f8950be01fb128b4fa6b6e2721f047ac9c2ec152
Sydney B <BestLemon555@gmail.com>
2022-09-01 19:37:21 -0700

    Getting readings from Gyro, setting stop and start motors, and updating field position

--- AZRL9413/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ AZRL9413/src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -9,8 +9,12 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import com.arcrobotics.ftclib.kinematics.wpilibkinematics.SwerveDriveKinematics;
 
 public class SwerveDriveSubsystem extends SubsystemBase {
   /** Creates a new SwerveDriveSubsystem. */
@@ -68,8 +72,41 @@ private final SwerveModule backRight = new SwerveModule(
   public void zeroHeading() {
     gyro.reset();
 }
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+public double getHeading() {
+  return Math.IEEEremainder(gyro.getAngle(), 360);
+}
+
+public Rotation2d getRotation2d() {
+  return Rotation2d.fromDegrees(getHeading());
+}
+
+public Pose2d getPose() {
+  return odometer.getPoseMeters();
+}
+
+public void resetOdometry(Pose2d pose) {
+  odometer.resetPosition(pose, getRotation2d());
+}
+@Override
+public void periodic() {
+    odometer.update(getRotation2d(), frontLeft.getState(), frontRight.getState(), backLeft.getState(),
+            backRight.getState());
+    SmartDashboard.putNumber("Robot Heading", getHeading());
+    SmartDashboard.putString("Robot Location", getPose().getTranslation().toString());
+}
+
+public void stopModules() {
+    frontLeft.stop();
+    frontRight.stop();
+    backLeft.stop();
+    backRight.stop();
+}
+
+public void setModuleStates(SwerveModuleState[] desiredStates) {
+    // SwerveDriveKinematics.normalizeWheelSpeeds(desiredStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+    frontLeft.setDesiredState(desiredStates[0]);
+    frontRight.setDesiredState(desiredStates[1]);
+    backLeft.setDesiredState(desiredStates[2]);
+    backRight.setDesiredState(desiredStates[3]);
+}
 }
